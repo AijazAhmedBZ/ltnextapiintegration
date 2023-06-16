028ebd707ea754674c52ea429adb522c6c2540ca
aijazahmedbz <aijaz.ahmed.bz@gmail.com>
2023-06-16 10:33:12 +0500

    updated on 16-Jun-23

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,33 @@
-import Image from 'next/image'
+interface IquoteData {
+  _id: string;
+  content: string;
+  author: string;
+  tags: string[];
+  authorSlug: string;
+  length: number;
+  dateAdded: string;
+  dateModified: string;
+}
 
-export default function Home() {
+const getData = async () => {
+  //error handling - try catch
+  try {
+    const res = await fetch("https://api.quotable.io/random?tags=technology", {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+export default async function Home() {
+  const quoteData: IquoteData = await getData();
   return (
     <>
-    <div>Aoa</div></>
-    )
+      <div>{quoteData.content}</div>
+    </>
+  );
 }
